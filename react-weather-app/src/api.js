b49b81e52d1520942b463ce69a3ddaefcef6e3b1
Zilin Song <harry.song518@gmail.com>
2024-05-11 18:10:59 +0930

    added api

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,8 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3ba1d9bfc9msh6861da74276dcd9p19520ajsn219a9aa04314',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
--- react-weather-app/src/components/search/search.js
+++ react-weather-app/src/components/search/search.js
@@ -1,9 +1,27 @@
 import {AsyncPaginate} from "react-select-async-paginate";
 import {useState} from "react";
+import {GEO_API_URL, geoApiOptions} from "../../api";
 
 const Search = ({onSearchChange}) => {
     const [search, setSearch] = useState(null);
+    const loadOptions = async (inputValue) => {
+        try {
+            const response = await fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions);
+            const result = await response.json();
 
+            return {
+                options: result.data.map((city) => {
+                    return {
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name}`,
+                    };
+                }),
+            };
+        } catch (error) {
+            console.error(error);
+            return { options: [] };
+        }
+    };
     const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
@@ -14,7 +32,7 @@ const Search = ({onSearchChange}) => {
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
-
+            loadOptions={loadOptions}
         />
     )
 }
