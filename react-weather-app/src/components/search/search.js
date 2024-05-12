bfd96bb9eb0fe8d0a7c63ed35138b06ed0e17166
Zilin Song <harry.song518@gmail.com>
2024-05-12 15:51:13 +0930

    fixed bug of city api

--- react-weather-app/src/components/search/search.js
+++ react-weather-app/src/components/search/search.js
@@ -4,24 +4,40 @@ import {GEO_API_URL, geoApiOptions} from "../../api";
 
 const Search = ({onSearchChange}) => {
     const [search, setSearch] = useState(null);
-    const loadOptions = async (inputValue) => {
-        try {
-            const response = await fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions);
-            const result = await response.json();
-
+    const loadOptions = (inputValue) => {
+      return fetch(
+        `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions
+      )
+      .then ((response) => response.json())
+      .then((response) => {
+        return {
+          options: response?.data?.map((city) => {
             return {
-                options: result.data.map((city) => {
-                    return {
-                        value: `${city.latitude} ${city.longitude}`,
-                        label: `${city.name}`,
-                    };
-                }),
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}`,
             };
-        } catch (error) {
-            console.error(error);
-            return { options: [] };
-        }
+          }),
     };
+  }).catch((err) => console.log(err.message));
+};
+    // const loadOptions = async (inputValue) => {
+    //     try {
+    //         const response = await fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions);
+    //         const result = await response.json();
+    //         // console.log(result);
+    //         return {
+    //             options: result.data.map((city) => {
+    //                 return {
+    //                     value: `${city.latitude} ${city.longitude}`,
+    //                     label: `${city.name}`,
+    //                 };
+    //             }),
+    //         };
+    //     } catch (error) {
+    //         console.error(error);
+    //         return { options: [] };
+    //     }
+    // };
     const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
