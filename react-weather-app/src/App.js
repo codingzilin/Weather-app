30c12af20dfde3246fb2196275205d825ba8f994
Zilin Song <harry.song518@gmail.com>
2024-05-18 14:40:13 +0930

    chore: Add CurrentWeather and forecast components, update API URL

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Search from "./components/search/search";
 import CurrentWeather from './components/current-weather/CurrentWeather';
+import forecast from './components/forecast/forecast';
 function App() {
-    const handleOnSearchChange = (searchData) => {
+  const [CurrentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
         console.log(searchData);
     }
     return (
     <div className="Container">
       <Search onSearchChange={handleOnSearchChange} />
       <CurrentWeather />
+      <forecast />
     </div>
   );
 }
